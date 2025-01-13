7d89efcf272f326bfaeec589abdd74b585640a7c
LLPFP <lopezpuigluis@fpllefia.com>
2025-01-13 17:45:21 +0100

    Set #/home en main

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import "./scss/styles.scss";
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
 
 import { header } from "./componentes/Header";
 import { footer } from "./componentes/Footer";
 
 // Importamos la Función para detectar eventos al cargar las vistas
-import { enrutador } from './componentes/enrutador'
+import { enrutador } from "./componentes/enrutador";
 
 //Inyectamos el componente header
 document.querySelector("header").innerHTML = header.template;
 document.querySelector("footer").innerHTML = footer.template;
 
-
 // Cargamos la vista por defecto (home)
-enrutador.observadorRutas()
+enrutador.observadorRutas();
 // Cargamos la página home
-window.location = '#/misProyectos'
+window.location = "#/home";
